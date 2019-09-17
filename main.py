6bb0c96e2773224f63cd2d36927695605fe722fe
homybee <55458993+homybee@users.noreply.github.com>
2019-09-17 22:29:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# Python Easy - Homework 1
+# Attributes of my favourite song
+# ---------------------------------
+# Defining and Declaring Variables
+Title = "Title : Where Is the Love"
+Artist = "Artist : The Black Eyed Peas"
+Album = "Album : Elephunk"
+Released = "Date : June 16, 2003"
+Genre = "Genre: Pop"
+Lenght = "Duration : 4min"
+Label = "Label: A&M, will.i.am, Interscope"
+# Assigning each songwriter and producers on a new line using \n
+Songwriters = "SongWriters:\n 1. will.i.am\n 2. apl.de.ap\n 3.Taboo\n 4. Justin Timberlake"
+Producers = "Producers:\n 1. will.i.am\n 2. Ron Fair"
+# defining a function to contain all attributes
+
+
+def FavouriteSong():
+    print("Attributes of Song")  # A heading 
+    print(Title)
+    print(Artist)
+    print(Album)
+    print(Released)
+    print(Genre)
+    print(Lenght)
+    print(Label)
+    print(Songwriters)
+    print(Producers)   
+FavouriteSong()
+
+print(Title) # Option to print any item of the attributes
+print(Songwriters)
